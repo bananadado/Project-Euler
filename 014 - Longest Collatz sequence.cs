7744e3d53f3670154d0da48b3598f32f3bdbcceb
Leonardo Matteucci <117661364+bananadado@users.noreply.github.com>
2023-01-14 20:06:32 +0000

    Add files via upload

---
+++ 014 - Longest Collatz sequence.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014___Longest_Collatz_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int highest = 0;
+            int highestChain = 0;
+            for (int i = 2; i < 1000000; i++)
+            {
+                long num = i;
+                int chain = 0;
+                while (num != 1)
+                {
+                    if (num % 2 == 0)
+                    {
+                        num /= 2;
+                    }
+                    else
+                    {
+                        num = num * 3 + 1;
+                    }
+                    chain++;
+                }
+                if (chain > highestChain)
+                {
+                    highest = i;
+                    highestChain = chain;
+                }
+            }
+            Console.WriteLine(highest);
+            Console.ReadKey();
+        }
+    }
+}
